989ff894bcb646c0cacaff6ceee2f41d103f527a
Hoang Van Quoc Nhat <hoangnhat250606@gmail.com>
2026-09-07 20:57:28 +0700

    BaiTapVongLap

---
+++ BaiTapVongLap/src/bai2.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var total = 0
+    for (i in 1..100) {
+        total += i
+    }
+    println("Tổng từ 1 - 100 là $total")
+}
---
+++ BaiTapVongLap/src/bai3.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var i = 1
+    while(i <= 20) {
+        if (i % 2 == 0)
+            println(i)
+        i++
+    }
+}
